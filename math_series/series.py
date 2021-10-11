b1ec2c4f603989a17a9f74870f11a787b125905b
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-11 21:13:46 +0300

    build: Sum function creation

--- math_series/series.py
+++ math_series/series.py
@@ -6,7 +6,7 @@ def fibonacci(n):
     for i in range(0, n):
         number = array[i] + array[i+1]
         array.append(number)
-    return str(array[n+1])
+    return str(array[n-1])
 
 
 def lucas(n):
@@ -17,4 +17,27 @@ def lucas(n):
     for i in range(0, n):
         number = array[i] + array[i+1]
         array.append(number)
-    return str(array[n+1])
+    return str(array[n-1])
+
+
+number_one = 0
+number_two = 1
+
+
+def sum_series(n, number_one, number_two):
+    """
+    Sum series function with one required parameter and two optional parameters. The required parameter will determine which element in the series to print. The two optional parameters will have default values of 0 and 1 and will determine the first two values for the series to be produced.
+    """
+    array = [number_one, number_two]
+    for i in range(0, n):
+        number = array[i] + array[i+1]
+        array.append(number)
+    return str(array[n-1])
+
+
+if __name__ == "__main__":
+    """
+    fibonacci( )
+    lucas( )
+    sum_series( )
+    """
