361dc5c5e0066087c8fd4a6e7f4a8c3220b5e440
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-11 21:23:23 +0300

    test: fibonacci function passed

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -1,6 +1,12 @@
 from math_series import __version__
-from math_series.series import fibonacci
+from math_series.series import fibonacci, lucas, sum_series
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_fibonacci():
+    actual = fibonacci(5)
+    expected = "3"
+    assert actual == expected
