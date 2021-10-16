de3f19582b630f965cd4ec249170b7d23c1aead5
Faisal-Kushha <fkushha89@hotmail.com>
2021-10-16 16:38:10 +0300

    Add Doc String

--- tests/test_math_series.py
+++ tests/test_math_series.py
@@ -6,25 +6,48 @@ def test_version():
     assert __version__ == '0.1.0'
 
 
+"""
+Fibonacci Series test
+"""
+
+
 def test_fibonacci():
     actual = fibonacci(5)
-    expected = "3"
+    expected = 5
     assert actual == expected
+
+
+"""
+Lucas Numbers test
+"""
 
 
 def test_lucas():
     actual = lucas(6)
-    expected = "11"
+    expected = 18
     assert actual == expected
 
 
+"""
+Sum series tests with and without the two optional parameters
+"""
+
+
 def test_sum_series1():
-    actual = sum_series(6, 2, 1)
-    expected = "11"
+    actual = sum_series(4, 2, 1)
+    print(actual)
+    expected = 7
     assert actual == expected
 
 
 def test_sum_series2():
-    actual = sum_series(7, 0, 1)
-    expected = "8"
+    actual = sum_series(7)
+    expected = 13
+    assert actual == expected
+
+
+def test_sum_series3():
+    actual = sum_series(3, 4, 5)
+    print(actual)
+    expected = 14
     assert actual == expected
